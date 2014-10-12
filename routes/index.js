cd5eff63938dac497266dee0eb54ef7d370939e7
Simon Buist <simon.buist@gmail.com>
2014-10-12 18:21:38 +0100

    Implement the mongodb addon in the tutorial

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-var registeredUsers = ["Ivan", "Natalie"];
-
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', users : registeredUsers});
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e, docs) {
+    res.render('index', { 
+      title : 'Express', 
+      users : docs 
+    });
+  });
+});
+
+/* GET userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e, docs) {
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+/* GET New user page */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add new user' });
 });
 
 router.post('/add',function(req, res) {
@@ -13,6 +34,37 @@ router.post('/add',function(req, res) {
     registeredUsers.push(userName);
     res.render('index', { title: 'Express', users : registeredUsers});
      
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
 });
 
 var validateUserName = function(username) {
